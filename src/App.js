26ec0c6cc9236cb4776ab8ed34d6fb223867540d
lubin <lu.bin.lu.bin@gmail.com>
2019-02-01 09:34:00 -0500

    move global styles of App into scoped one

--- src/App.js
+++ src/App.js
@@ -1,29 +1,81 @@
-import React from 'react';
+import React from "react";
 
 // use the line below instead
 // import styled from 'styled-components';
 
 //this line enables babel-plugin-styled-components
-import styled from 'styled-components/macro';
+import styled, { keyframes } from "styled-components/macro";
 
-const Title = styled.h1 `
+import logo from './logo.svg';
+import './App.css';
+
+const Applogospin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+
+  .App-header {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+
+  .App-logo {
+    animation: ${Applogospin} infinite 20s linear;
+    height: 40vmin;
+
+    &:hover {
+      animation: ${Applogospin} infinite 1.5s linear;
+    }
+  }
+
+  // global
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
 `;
 
-const Wrapper = styled.section `
+const Wrapper = styled.section`
   padding: 4em;
   background: papayawhip;
 `;
 
 function App() {
   return (
-  <Wrapper> 
-    <Title>
-      Hello World!
-    </Title>
-  </Wrapper>);
+    <AppWrapper>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </AppWrapper>
+  );
 }
 
 export default App;
