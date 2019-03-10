8b87aac58b176d6139308c86e9f8bf3f62b3867d
lubin <lu.bin.lu.bin@gmail.com>
2019-03-10 17:21:45 -0400

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,81 +1,25 @@
-import React from "react";
-
-// use the line below instead
-// import styled from 'styled-components';
+import React from 'react';
 
 //this line enables babel-plugin-styled-components
-import styled, { keyframes } from "styled-components/macro";
+import styled from 'styled-components/macro';
 
-import logo from './logo.svg';
-import './App.css';
-
-const Applogospin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const AppWrapper = styled.div`
-  text-align: center;
-
-  .App-header {
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-  }
-
-  .App-logo {
-    animation: ${Applogospin} infinite 20s linear;
-    height: 40vmin;
-
-    &:hover {
-      animation: ${Applogospin} infinite 1.5s linear;
-    }
-  }
-
-  // global
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-`;
-
+// Create a Title component that'll render an <h1> tag with some styles
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
 `;
 
+// Create a Wrapper component that'll render a <section> tag with some styles
 const Wrapper = styled.section`
   padding: 4em;
   background: papayawhip;
 `;
 
-function App() {
+export default function App() {
   return (
-    <AppWrapper>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </AppWrapper>
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
   );
 }
-
-export default App;
